3098544644b739517cd696931e8c67a137ce1aad
Cecília Candeia <ceciliacanlyra@gmail.com>
2026-06-04 00:51:07 +0000

    questão 8

--- q7.py
+++ q7.py
@@ -4,7 +4,10 @@ forma_pagamento = input("Informe a forma de pagamento (Dinheiro/Cheque): ")
 if valor_produto >= 100.0 and forma_pagamento == "Dinheiro":
     valor_produto = valor_produto - (0.1 * valor_produto)
     print(f"R$ {valor_produto:.2f}")
-elif forma_pagamento == "Cartão":
+
+elif forma_pagamento != "Dinheiro" and forma_pagamento != "Cheque":
     print("Forma de pagamento inválida")
+    
 else:
     print(f"R$ {valor_produto:.2f}")
+
