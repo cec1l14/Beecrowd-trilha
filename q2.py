c405966e47c610f637f771eb589110d454a69846
Cecília Candeia <ceciliacanlyra@gmail.com>
2026-06-04 00:52:01 +0000

    esqueci o f

--- q2.py
+++ q2.py
@@ -3,5 +3,5 @@ h = float(input("Informe sua altura: "))
 peso_homens = (72.7 * h) - 58
 peso_mulheres = (62.1 * h) - 44.7
 
-print("Peso ideal para homens: {peso_homens:.2f} kg")
-print("Peso ideal para mulhere: {peso_mulheres:.2f} kg")
+print(f"Peso ideal para homens: {peso_homens:.2f} kg")
+print(f"Peso ideal para mulhere: {peso_mulheres:.2f} kg")
