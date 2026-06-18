2a2501cbf17bb3500ceae5778efbd11c0914b381
Cecília Candeia <ceciliacanlyra@gmail.com>
2026-06-18 13:46:08 +0000

    questao 1070

--- q1.py
+++
@@ -1,5 +0,0 @@
-raio = float(input("Informe o raio do círculo: "))
-
-area = 3.14 * (raio * raio)
-
-print(f"Área = {raio:.2f}")
--- q2.py
+++
@@ -1,7 +0,0 @@
-h = float(input("Informe sua altura: "))
-
-peso_homens = (72.7 * h) - 58
-peso_mulheres = (62.1 * h) - 44.7
-
-print(f"Peso ideal para homens: {peso_homens:.2f} kg")
-print(f"Peso ideal para mulhere: {peso_mulheres:.2f} kg")
